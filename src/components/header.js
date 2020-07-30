80f7a4a5496bc72a93d2deadfd78dfe5fb0a5a75
dw-ll <danswilliams95@gmail.com>
2020-07-30 12:50:18 -0700

    Add mobile sotd.

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,32 @@
 import React from "react"
 import tw, { styled } from "twin.macro"
 import Logo from '../images/koodos_logo.png'
+import Avatar from '../images/kody_mint.png'
 import { Icon } from '@iconify/react';
 import userSolid from '@iconify/icons-clarity/user-solid';
 import bxSearchAlt from '@iconify/icons-bx/bx-search-alt';
 
-const Head = styled.header`${tw`sticky bg-main-bg`}`
-const Nav = styled.div`${tw`flex items-center justify-between w-full mx-auto px-4 py-1`}`
-const NavImageWrapper = styled.div`${tw`flex justify-start w-1/3`}`
-const NavImage = styled.img`${tw`w-auto h-10`}`
-const NavSearch = styled.span`${tw`flex flex-grow justify-start px-2 mx-auto py-2  focus:shadow-lg focus:outline-none`}`
-const SearchInput = styled.input`${tw`w-1/2 py-2 flex text-center justify-center rounded-md shadow-md bg-main-fg text-secondary-text transition-colors duration-100 ease-in-out`}`
-const NavIcons = styled.div`${tw`flex items-center content-center mr-2 space-x-4`}`
+const Head = styled.header`${tw`mx-auto bg-main-bg`}`
+const Nav = styled.div`${tw`flex items-center justify-between w-full mx-auto px-4 py-1 z-40`}`
+const NavImageWrapper = styled.div`${tw`flex justify-start w-1/4 lg:w-48 py-2 px-2 `}`
+const NavImage = styled.img`${tw`hidden md:block w-auto h-10`}`
+const NavAvatar = styled.img`${tw`md:hidden w-auto h-16`}`
+const NavSearch = styled.span`${tw`flex w-1/2 mx-auto  py-2 lg:pl-4 items-start justify-start  focus:shadow-lg focus:outline-none`}`
+const SearchInput = styled.input`${tw`w-full px-4 py-2 text-center justify-center rounded-md shadow-md bg-main-fg text-secondary-text transition-colors duration-100 ease-in-out`}`
+const NavIcons = styled.div`${tw`flex items-center content-center mx-auto mr-2 `}`
 
 const Header = ({ siteTitle }) => (
   <Head>
     <Nav>
       <NavImageWrapper>
         <NavImage src={Logo} />
+        <NavAvatar src={Avatar} />
       </NavImageWrapper>
+
       <NavSearch>
         <SearchInput placeholder="search for curations + curators" />
       </NavSearch>
+
       <NavIcons>
         <a href='/'>
           <Icon height="31" width="31" color="#47EED0" icon={userSolid} />
@@ -31,6 +36,6 @@ const Header = ({ siteTitle }) => (
         </a>
       </NavIcons>
     </Nav>
-  </Head>
+  </Head >
 )
 export default Header
--- src/components/sotd.js
+++ src/components/sotd.js
@@ -2,39 +2,71 @@ import React from "react"
 import ReactPlayer from 'react-player'
 import Typist from 'react-typist';
 import TextLoop from "react-text-loop";
-import tw from "twin.macro"
+import tw, { styled } from "twin.macro"
 
 
 // Styled Containers
 const ContentContainer = tw.section`h-full mx-auto items-center bg-main-bg flex flex-col space-y-2`
-const SOTDContainer = tw.article`relative rounded-md shadow-lg mx-24 mt-4 h-128 bg-main-fg`
-const VOTDContainer = tw.div`px-12 py-3 pt-6 z-20  flex items-center`
+const SOTDContainer = styled.article`
+${tw`relative md:hidden bg-main-fg rounded-lg shadow-md`}
+width:345px;
+height:415px;
+`
+const LargeSOTDContainer = tw.article`hidden md:block relative rounded-md shadow-lg mx-64 md:mx-24 mt-4 h-128 bg-main-fg`
+const VOTDContainer = tw.div`px-8 pt-16 flex items-center`
+const LargeVOTDContainer = tw.div`px-8 py-3 pt-6 z-20 flex items-center`
+
 
 
 // Styled Wrappers
-const SOTDHeaderWrapper = tw.span`flex justify-start w-full px-40 py-3`
-const SOTDTextWrapper = tw.span`absolute bottom-0 left-0 mb-24 -ml-16 flex flex-col`
-const SOTDCurationsWrapper = tw.article`flex justify-center space-x-4`
+const SOTDHeaderWrapper = tw.span`absolute top-0 left-0 px-4 py-2 `
+const LargeSOTDHeaderWrapper = tw.span`flex justify-center  md:justify-start w-full mx-16 px-12 md:mx-0 md:px-40 py-3`
+const SOTDTextWrapper = tw.span`absolute bottom-0 left-0 mb-6 md:mb-24 ml-4 md:-ml-16 flex flex-col`
+const SOTDCurationsWrapper = tw.article`hidden md:flex justify-center space-x-4 py-3`
 
 // Styled Text + Buttons
-const SOTDHeader = tw.h1`text-left font-semibold text-white text-4xl tracking-wide border-b-2  border-main-accent`
-const SOTDText = tw.h1`transition ease-in duration-700 font-semibold text-white text-5xl z-20`
-const SOTDCurText = tw.h1`font-semibold text-main-accent text-5xl z-20 ml-2`
+const LargeSOTDHeader = tw.h1`text-center md:text-left font-semibold text-white text-xl md:text-4xl tracking-wide border-b-2 border-main-accent`
+const SOTDText = tw.h1`transition ease-in duration-700 font-semibold text-white text-3xl md:text-5xl z-20`
+const SOTDCurText = tw.h1`font-semibold text-main-accent text-3xl md:text-5xl z-20 ml-2 underline`
 const SOTDSubtext = tw.span`flex justify-between`
-const SOTDCurationsText = tw.article`font-semibold text-white text-4xl m-1`
-const SOTDCurateButton = tw.button`absolute z-20 bottom-0 right-0 ml-10 -mb-8 px-10 py-3 bg-main-accent font-semibold text-lg text-black`
-const SOTDCurateButtonBG = tw.button`absolute z-10 bottom-0 right-0 ml-16 -mb-10 px-10 py-3 bg-black font-semibold text-lg text-black`
+
+const SOTDCurationsText = tw.article`font-semibold text-white text-2xl md:text-4xl m-1`
+const SOTDCurateButton = styled.button`
+${tw`absolute bottom-0 right-0 ml-8 -mb-6  py-3 rounded-full shadow-lg bg-main-accent text-lg`}
+height:65px;
+width:65px;
+`
+const LargeSOTDCurateButton = tw.button`absolute z-20 bottom-0 right-0 ml-10 -mb-8 px-10 py-3 bg-main-accent font-semibold text-lg text-black`
+const LargeSOTDCurateButtonBG = tw.button`absolute z-10 bottom-0 right-0 ml-16 -mb-10 px-10 py-3 bg-black font-semibold text-lg text-black`
 
 
 const SOTD = () => (
     <ContentContainer>
-        <SOTDHeaderWrapper>
-            <SOTDHeader><Typist startDelay={1000}>👋🏻  🎧 discover new music + curators through emojis. </Typist></SOTDHeader>
-        </SOTDHeaderWrapper>
+        <LargeSOTDHeaderWrapper>
+            <LargeSOTDHeader><Typist startDelay={1000}>👋🏻  🎧 discover new music + curators through emojis. </Typist></LargeSOTDHeader>
+        </LargeSOTDHeaderWrapper>
         <SOTDContainer>
+            <SOTDHeaderWrapper>
+                <SOTDText>song of the day</SOTDText>
+            </SOTDHeaderWrapper>
             <VOTDContainer>
-                <ReactPlayer width='900px' height='460px' controls={true} url='https://www.youtube.com/watch?v=Tw0zYd0eIlk' />
+                <ReactPlayer width='350px' height='230px' controls={true} url='https://www.youtube.com/watch?v=Tw0zYd0eIlk' />
             </VOTDContainer>
+            <SOTDTextWrapper>
+                <SOTDText>
+                    phoebe bridgers
+                </SOTDText>
+                <SOTDSubtext>
+                    <SOTDText>kyoto</SOTDText>
+                    <SOTDCurText>12 curations</SOTDCurText>
+                </SOTDSubtext>
+            </SOTDTextWrapper>
+            <SOTDCurateButton>🎧</SOTDCurateButton>
+        </SOTDContainer>
+        <LargeSOTDContainer>
+            <LargeVOTDContainer>
+                <ReactPlayer width='900px' height='460px' controls={true} url='https://www.youtube.com/watch?v=Tw0zYd0eIlk' />
+            </LargeVOTDContainer>
             <SOTDTextWrapper>
                 <SOTDText>song of the day</SOTDText>
                 <SOTDText>phoebe bridgers</SOTDText>
@@ -43,9 +75,9 @@ const SOTD = () => (
                     <SOTDCurText>12 curations</SOTDCurText>
                 </SOTDSubtext>
             </SOTDTextWrapper>
-            <SOTDCurateButton>curate this</SOTDCurateButton>
-            <SOTDCurateButtonBG>curate this</SOTDCurateButtonBG>
-        </SOTDContainer>
+            <LargeSOTDCurateButton>curate this</LargeSOTDCurateButton>
+            <LargeSOTDCurateButtonBG>curate this</LargeSOTDCurateButtonBG>
+        </LargeSOTDContainer>
         <SOTDCurationsWrapper>
             <TextLoop delay={500} mask={true} fade={false}>
                 <SOTDCurationsText>🥺👉👈</SOTDCurationsText>
@@ -59,8 +91,6 @@ const SOTD = () => (
                 <SOTDCurationsText>👩‍🎤🚝🎸</SOTDCurationsText>
             </TextLoop>
         </SOTDCurationsWrapper>
-
-
     </ContentContainer>
 )
 export default SOTD;
